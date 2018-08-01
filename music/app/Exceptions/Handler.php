928f13e41689093f4e68837ab90d116f85ce2aba
uveysservetoglu <uveysservetoglu@gmail.com>
2018-08-01 17:17:11 +0300

    Eğer login değilse mesaj

--- music/app/Exceptions/Handler.php
+++ music/app/Exceptions/Handler.php
@@ -46,6 +46,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceof \Illuminate\Auth\AuthenticationException ){
+
+            return response('Please login', 401);
+
+        }
         return parent::render($request, $exception);
     }
 }
